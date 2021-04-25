29296e925a32564ebdc83c10e9a1b1556c240a62
Paul <paul.kirn@yandex.ru>
2021-04-25 21:44:08 +0300

    Multi-file programs, cpp and h files. The preprocessor and its directives.

--- SuperProject/Main.cpp
+++ SuperProject/Main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+//#include "Math.h"
+#define PI 3.14
+//#define LOG
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	/*int result = sum(10, 20);
+	std::cout << "Result = " << result << std::endl;*/
+
+#ifdef LOG
+	std::cout << "log message" << std::endl;
+#endif // LOG
+	std::cout << "pi = " << PI << std::endl;
+	return 0;
 }
