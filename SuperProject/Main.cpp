4a5b3886cf1326f0465f1c5b657f8c671b091636
Paul <paul.kirn@yandex.ru>
2021-05-16 20:08:52 +0300

    17.1 Classes Description and Creating Objects in C ++

--- SuperProject/Main.cpp
+++ SuperProject/Main.cpp
@@ -1,11 +1,30 @@
-#include <iostream>
-#include <stdint.h>
-#include <iomanip>
-#include <string>
 
+#include <iostream>
+
+using namespace std;
+
+
+class Example
+{
+private:
+	int a;
+public:		
+	int GetA()
+	{
+		return a;
+	}
+
+	void SetA(int newA)
+	{
+		a = newA;
+	}
+};
 
 int main()
 {
-	bool a = false;
-	std::cout << (a || true);
+	Example temp, temp1;
+	temp.SetA(5);
+	temp1.SetA(10);
+	cout << temp.GetA() << '\n' << temp1.GetA();
+	
 }
