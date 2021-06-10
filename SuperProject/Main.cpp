125b394a303267668c20e961ba2e7c9cfa7df5b4
Paul <paul.kirn@yandex.ru>
2021-06-10 12:29:38 +0300

    19.3 Templates

--- SuperProject/Main.cpp
+++ SuperProject/Main.cpp
@@ -3,49 +3,49 @@
 
 using namespace std;
 
+//template<class T>
+//T sum(T a, T b)
+//{
+//	return a + b;
+//}
+
+template<class T, class M>
 class A
 {
-protected:
-	int x;
+	T value;
+	M anotherValue;
 public:
 	A() {}
-	A(int _x) : x(_x)
-	{}
-	int GetX()
+	A(T _value) : value(_value) {}
+
+	void ShowValue()
 	{
-		return x;
+		cout << value;
 	}
 
-	virtual void Show()
+	void SetValue(T newValue)
 	{
-		cout << "A";
+		value = newValue;
+	}
+
+	void SetAnotherValue(M newAnotherVal)
+	{
+		anotherValue = newAnotherVal;
 	}
 };
 
-class B : public A
-{
-private:
-	double y;
-public:
-	B() {}
-	B(double _y, int _x) : y(_y), A(_x)
-	{}
-	double GetY()
-	{
-		return x + y;
-	}
-
-	void Show() override
-	{
-		cout << "B";
-	}
-};
 
 int main()
 {
-	A* p = new B(5.2, 2);
-	/*cout << p->GetX() << ' ' << p->GetY();*/
-	p->Show();
+	/*sum<int>(1, 2);
+	sum<float>(2.f, 3.f);
+	sum<double>(2.0, 3.0);*/
+
+	A<double, int>* ptrInt = new A<double, int>(10.6);
+	ptrInt->ShowValue();
+	ptrInt->SetValue(2.1);
+	ptrInt->ShowValue();
+	
 }
 
 
